03989a8bb1a6eec8ad3acd7f32bbcd7d31990850
livepoint <tom.gray@livepointuk.com>
2016-04-21 22:32:06 +0100

    - Added initial drafts of some date and time utils. - Added an experimental composer.json file for using the repo with composer.

---
+++ Library/Utils/DaysEnum.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Greasy Lab.
+ * 
+ * Thursday 21st April 2016
+ * 
+ * Tom Gray
+ */
+
+namespace GreasyLab\Library\Utils;
+
+/**
+ * Defines constants for the ISO-8601 numeric representations of the day of the
+ * week (added in PHP 5.1.0).
+ * 1 for Monday through to 7 for Sunday.
+ * 
+ * @author Tom Gray
+ */
+interface DaysEnum
+{
+    
+    /**
+     * The ISO-8601 numeric representation for Monday.
+     * 
+     * @var int
+     */
+    const MON = 1;
+    
+    /**
+     * The ISO-8601 numeric representation for Tuesday.
+     * 
+     * @var int
+     */
+    const TUE = 2;
+    
+    /**
+     * The ISO-8601 numeric representation for Wednesday.
+     * 
+     * @var int
+     */
+    const WED = 3;
+    
+    /**
+     * The ISO-8601 numeric representation for Thursday.
+     * 
+     * @var int
+     */
+    const THU = 4;
+    
+    /**
+     * The ISO-8601 numeric representation for Friday.
+     * 
+     * @var int
+     */
+    const FRI = 5;
+    
+    /**
+     * The ISO-8601 numeric representation for Saturday.
+     * 
+     * @var int
+     */
+    const SAT = 6;
+    
+    /**
+     * The ISO-8601 numeric representation for Sunday.
+     * 
+     * @var int
+     */
+    const SUN = 7;
+}
---
+++ Library/Utils/HoursManager.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Greasy Labs.
+ * 
+ * Thursday 21st April 2016
+ * 
+ * Tom Gray
+ */
+
+namespace GreasyLab\Library\Utils;
+
+/**
+ * 
+ *
+ * @author Tom Gray
+ */
+class HoursManager
+{
+    
+}
