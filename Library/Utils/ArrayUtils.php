ef17217ac18fb7404515b2e7d22119b3e42b0041
tom <tomgray@coffeefuelled.co.uk>
2016-12-01 11:59:27 +0000

    Array Utils - Implemented the method to create all unique combinations of elements from   multiple arrays.

--- Library/Utils/ArrayUtils.php
+++ Library/Utils/ArrayUtils.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * 
+ * Copyright (c) 2016 Digital Grease Limited.
  * 
  * Thursday 10th November 2016
  * 
@@ -11,16 +11,56 @@
 namespace DigitalGrease\Library\Utils;
 
 /**
- * 
+ * Utility methods to manipulate arrays.
  *
  * @author Tom Gray
  */
 class ArrayUtils
 {
     
-    public function createCombos()
+    /**
+     * Create an array of all unique combinations of elements that can be
+     * created from the elements of multiple arrays.
+     * This is a variadic function that will work with two or more arrays.
+     * 
+     * @param array $array1
+     * @param array $array2
+     * 
+     * @return array
+     */
+    public function createCombinations(array $array1, array $array2)
     {
-        // DO TG Implement Algorithm
+        $combinations = [];
+        
+        $totalCombinations = 1;
+        foreach (func_get_args() as $array) {
+            $totalCombinations *= count($array);
+        }
+        
+        // Initialise the indexes for accessing the arrays to build the
+        // combinations.
+        $indexes = [];
+        for ($i = 0; $i < func_num_args(); ++$i) {
+            $indexes[] = 0;
+        }
+        
+        for ($i = 0; $i < $totalCombinations; ++$i) {
+            $elements = [];
+            foreach (func_get_args() as $a => $array) {
+                $elements[] = $array[$indexes[$a]];
+            }
+            $combinations[] = $elements;
+            
+            // Set the next index to create the next combination.
+            foreach (func_get_args() as $a => $array) {
+                if (++$indexes[$a] < count($array)) {
+                    break;
+                }
+                $indexes[$a] = 0;
+            }
+        }
+        
+        return $combinations;
     }
     
     /**
